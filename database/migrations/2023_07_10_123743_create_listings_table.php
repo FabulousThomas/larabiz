15c5b6e862d830ea0ff8615e9955357b629935df
Mba Thomas <angelfab631@gmail.com>
2023-07-10 15:04:19 +0100

    database files

---
+++ database/migrations/2023_07_10_123743_create_listings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('name');
+            $table->string('address');
+            $table->string('website');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('bio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('listings');
+    }
+};
