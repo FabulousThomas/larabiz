fcf5507e45552acc73e45c285af463356d2873e5
Mba Thomas <angelfab631@gmail.com>
2023-07-10 15:05:50 +0100

    route files

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+Route::resource('listings', 'App\Http\Controllers\ListingsController');
+Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index']);
